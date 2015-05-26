43da5a014fe63e50312a37404287d1b6891384ec
dale tan <dale@1stdibs.com>
2015-05-26 16:16:45 -0400

    initial commit after moving over from bunsen

---
+++ README.md
@@ -0,0 +1,24 @@
+# Find Namespace Value
+
+Find a value inside an object namespace
+
+example:
+
+    var findNamespaceValue = require('find-namespace-value');
+    var obj = {
+        company: {
+            name: '1stdibs'
+        }
+    };
+    var companyName = findNamespaceValue('company.name', obj);
+    var companyLocation = findNamespaceValue('company.location', obj, 'New York, NY');
+    var companySecrets = findNamespaceValue('company.secrets', obj);
+    
+    console.log(companyName); // => "1stdibs"
+    console.log(companyLocation); // => "New York, NY"
+    console.log(companySecrets); // => undefined
+
+testing:
+
+    $ npm install
+    $ npm test
---
+++ test/index.js
@@ -0,0 +1,48 @@
+/**
+ * User: daletan
+ * Date: 4/22/14
+ * Time: 10:25 AM
+ * Copyright 1stdibs.com, Inc. 2014. All Rights Reserved.
+ */
+
+'use strict';
+
+var assert = require('assert')
+var findNamespaceValue = require('..');
+
+describe("findNamespaceValue", function () {
+    var tester;
+    beforeEach(function () {
+        tester = {
+            test: {
+                name: {
+                    space: {}
+                }
+            }
+        };
+    });
+    afterEach(function () {
+        tester = null;
+    });
+    it('should find "test.name" and be equal to an empty object', function () {
+        var ns = findNamespaceValue('test.name', tester);
+        assert(ns, { space: { } });
+    });
+    it('should return the fallback value', function () {
+        var obj = {};
+        var ns = findNamespaceValue('noName', obj, 'this is the fallback');
+        assert.equal(ns, 'this is the fallback');
+    });
+    it('should return the value from an object', function () {
+        var obj = {
+            name: 'yoda'
+        };
+        var ns = findNamespaceValue('name', obj, 'this should not be the value');
+        assert.equal(ns, 'yoda');
+    });
+    it('should return the fallback value for non-object parent', function () {
+        var fallback = 'this should not be the value';
+        var ns = findNamespaceValue('name', true, fallback);
+        assert.equal(ns, fallback);
+    });
+});
